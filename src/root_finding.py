1ecf505ddb005b4f5b8f2752bf42fbba0b8526e3
charliepeyton <s2510966@ed.ac.uk>
2025-11-15 12:49:54 +0000

    Updated plot
    Updated plot labels and objective function for deflection.

--- src/root_finding.py
+++ src/root_finding.py
@@ -49,7 +49,7 @@ c=1000
 k_values_plot = np.linspace(500, 10000, 100) 
 
 
-# We use objective(k) which returns (deflection - 0.05)
+# We use objective(k) which returns (deflection - 0.025)
 objective_values = []
 for k_val in k_values_plot:
     print(f"Plotting: Trying k={k_val:.0f}")
@@ -57,23 +57,23 @@ for k_val in k_values_plot:
 
 #Create the plot
 plt.figure(figsize=(10, 6))
-plt.plot(k_values_plot, objective_values, 'b-', label='objective(k) [Max Deflection - 0.05 m]')
+plt.plot(k_values_plot, objective_values, 'b-')
 
 # Add the target line (where objective = 0)
 plt.axhline(y=0.0, color='r', linestyle='--', 
-            label='Target (objective = 0)')
+            label='Target = Maximum deflection is 0.025m ')
 
 #Add labels and title
 plt.xlabel('Spring Stiffness $k$ (N/m)')
-plt.ylabel('Objective Function Output')
-plt.title('Objective Function vs. Stiffness')
+plt.ylabel('Suspension deflection - 0.025 (m)')
+plt.title('Suspension Deflection vs. Stiffness')
 plt.legend()
 plt.grid(True)
-plt.savefig('objective_function_plot.png')
+plt.savefig('rootfinding_plot.png')
 
-print("Plot 'objective_function_plot.png' saved.")
-print("Look at this plot to find where the blue line crosses the red line.")
+print("Look at this plot to find where the blue line intersects the red line.")
 print("Use k-values on either side of that crossing as your new k0 and k1.")
+
 
 k0,k1=4000,6000
 k_solution= secant(objective,k0,k1)
